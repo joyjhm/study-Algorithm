273fb9e8ff79c096bf94581f3d13335e525f6e10
전현민 <77275989+joyjhm@users.noreply.github.com>
2023-10-10 18:10:59 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int stairs[301];
+int dp[301];
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cin >> stairs[i];
+    }
+    dp[1] = stairs[1];
+    dp[2] = stairs[1] + stairs[2];
+
+    for(int i = 3; i <= n; i++) {
+        dp[i] = max(dp[i - 3] + stairs[i - 1], dp[i - 2]) + stairs[i];
+    }
+
+    cout << dp[n];
+}
