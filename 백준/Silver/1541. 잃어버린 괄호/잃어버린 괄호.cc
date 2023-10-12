3c41b15a4ae2545c5ad8e97e7f94bc451ef64eba
전현민 <77275989+joyjhm@users.noreply.github.com>
2023-10-12 17:24:43 +0900

    주석 추가 잃어버린 괄호.cc

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,3 +1,8 @@
+/*
+    다른 표현 방법
+    파싱하지 않고 string의 index로 접근
+*/
+
 #include<iostream>
 #include<vector>
 #include<sstream>
@@ -19,19 +24,19 @@ vector<string> split(string str, char deli) {
 int main() {
     string s;
     cin >> s;
-    vector<string> minus = split(s, '-');
+    vector<string> minus = split(s, '-');    // - 연산자로 파싱
     for (int i=0; i < minus.size();i++){
-        vector<string> plus = split(minus[i], '+');
+        vector<string> plus = split(minus[i], '+'); // - 연산자 사이에 + 연산자들로 이루어진 vector를 만든다.
         int add = 0;
         for (int j = 0; j < plus.size(); j++) {
-            add += stoi(plus[j]);
+            add += stoi(plus[j]);    // add: + 연산자로 이루어진 값들을 더한다.
         }
         sum.push_back(add);
 
     }
-    int result = sum[0];
+    int result = sum[0];            // 처음 마이너스 만나기 전까지의 덧셈의 합은 더함.
     for(int i =1; i < sum.size(); i++) {
-        result -= sum[i];
+        result -= sum[i];        //  그 이후에 - 만나면 덧셈의 합들을 빼준다.
     }
 
     cout << result;
