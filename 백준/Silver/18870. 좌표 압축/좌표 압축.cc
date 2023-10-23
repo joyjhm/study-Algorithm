e3136b2b825acb16c1d7e33a58c97a46ca1c80e9
전현민 <77275989+joyjhm@users.noreply.github.com>
2023-10-23 22:20:08 +0900

    주석 추가 좌표 압축.cc

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,3 +1,11 @@
+/*
+같은 벡터를 2개 만들고 하나의 벡터에서 중복을 없애고 오름차순 정렬한다.
+vector에서 중복을 없애는 방법: cv.erase(unique(cv.begin(), cv.end()), cv.end());
+
+중복을 제외하고 정렬한 벡터에서 lower_bound(원하는 값 k 이상이 처음 나오는 위치를 찾는 과정)를 써서 좌표의 값을 구한다.
+lower_bound(v.begin(), v.end(), num) - v.begin();
+*/
+
 #include<iostream>
 #include<map>
 using namespace std;
