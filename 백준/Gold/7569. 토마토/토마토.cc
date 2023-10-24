864f7eb33b7da201be8062b945e5d8f51f709a08
전현민 <77275989+joyjhm@users.noreply.github.com>
2023-10-24 22:27:18 +0900

    [Gold V] Title: 토마토, Time: 260 ms, Memory: 7384 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int tomatoes[100][100][100];
+int dz[6] = {0,0,0,0,1,-1};
+int dy[6] = {0,1,0,-1,0,0};
+int dx[6] = {1,0,-1,0,0,0};
+int m, n, h;
+int days = 0;
+bool unable;
+
+struct Point{
+    int z;
+    int y;
+    int x;
+
+    Point(int z, int y, int x) {
+        this->z = z;
+        this->y = y;
+        this->x = x;
+    }
+
+};
+
+queue<Point> q;
+
+void bfs() {
+    while(!q.empty()){
+        int temp_z = q.front().z;
+        int temp_y = q.front().y;
+        int temp_x = q.front().x;
+        q.pop();
+        for(int s = 0; s < 6; s++) {
+            int x = temp_x + dx[s];
+            int y = temp_y + dy[s];
+            int z = temp_z + dz[s];
+            if(x < 0 || x >= m || y < 0 || y >= n || z < 0 || z >= h)
+                continue;
+            if(tomatoes[z][y][x] == 0) {
+                tomatoes[z][y][x] = tomatoes[temp_z][temp_y][temp_x] + 1;
+                q.push(Point(z,y,x));
+            }
+        }
+    }
+}
+
+int main() {
+
+    cin >> m >> n >> h;
+    for(int k = 0; k < h; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                cin >> tomatoes[k][i][j];
+                if (tomatoes[k][i][j] == 1) {
+                    q.push(Point(k,i,j));
+                }
+            }
+        }
+    }
+
+    bfs();
+
+
+    for(int k = 0; k < h; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(tomatoes[k][i][j] > days) {
+                    days = tomatoes[k][i][j];
+                }
+                if(tomatoes[k][i][j] == 0){
+                    unable = true;
+                }
+            }
+        }
+    }
+    if(unable) {
+        cout << -1;
+    }
+    else {
+        cout << days - 1;
+    }
+
+}
