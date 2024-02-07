9c2bcf9c08cf18c4376ca355bf7b1bc51a0439b1
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-02-07 15:24:26 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    
+    while(a != b) {
+        if (a % 2 == 0) {
+            a = a / 2;
+        } else {
+            a = a / 2 + 1;   
+         }
+        if (b % 2 == 0) {
+            b = b / 2;
+        } else {
+            b = b / 2 + 1;
+        }
+        answer++;
+    }
+    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << answer << endl;
+
+    return answer;
+}
