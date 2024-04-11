689a6e7a227a5af832169bd402ad997ce0876aed
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-04-12 02:03:46 +0900

    [Silver III] Title: 소수 구하기, Time: 496 ms, Memory: 38676 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,11 @@
+m, n = map(int, input().split())
+num = [1] * 1000001
+num[1] = 0
+for i in range(2,n + 1):
+    if num[i]:
+        for j in range(i * 2, n + 1, i):
+            num[j] = 0
+
+for i in range(m, n + 1):
+    if num[i]:
+        print(i) 
