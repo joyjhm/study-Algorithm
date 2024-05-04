249ffe3ec703c86b90285093312896e8e4a03b91
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-05-05 01:45:35 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.cc
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -12,60 +12,39 @@ int main() {
     string str;
     cin >> str;
 
-    int cnt = 0;
+
     for (int i = 0; i < str.length(); i++) {
-        if(str[i] >= 65 && str[i] <= 90) {
+        if(str[i] >= 'A' && str[i] <= 'Z') {
             cout << str[i];
+        }
+        else if(str[i] == '(') {
+            op.push({0, str[i]});
+        }
+        else if(str[i] == ')') {
+            if(str[i] == ')') {
+                while(op.top().second != '(') {
+                    cout << op.top().second;
+                    op.pop();
+                }
+                op.pop();
+            }
         }
         else {
             int p;
             if(str[i] == '+' || str[i] == '-') {
-                p = 0;
+                p = 1;
             } 
             else if(str[i] == '*' || str[i] == '/') {
-                p = 1;
-            }
-            else if(str[i] == '(') {
                 p = 2;
             }
 
-            if(str[i] == '(') {
-                op.push({p, str[i]});
-                cnt++;
+            while(!op.empty() && op.top().first >= p) {
+                cout << op.top().second;
+                op.pop();
             }
-            else if(op.empty()) {
-                op.push({p, str[i]});
-  
-            } 
-            else {
-                if(str[i] == ')') {
-                    while(op.top().second != '(') {
-                        cout << op.top().second;
-                        op.pop();
-                    }
-                    op.pop();
-                    cnt--;
-                }
-                else if(cnt > 0) {
+            op.push({p, str[i]});
 
-                    while(op.top().second != '(' && op.top().first >= p) {
-                        cout << op.top().second;
-                        op.pop();
-                    }
-                    op.push({p, str[i]});
-                }
 
-                else if(op.top().first < p) {
-                    op.push({p, str[i]});
-                }
-                else {
-                    while(!op.empty() && op.top().first >= p) {
-                        cout << op.top().second;
-                        op.pop();
-                    }
-                    op.push({p, str[i]});
-                }
-            }
         }
     }
 
