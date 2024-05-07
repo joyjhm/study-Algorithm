76c336cd1c208ca60ccd048b6ac4afa8138b8567
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-05-07 16:20:59 +0900

    [Gold III] Title: 파일 합치기, Time: 144 ms, Memory: 2880 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -1,39 +1,10 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cstring>
 #include <climits>
-
 using namespace std;
 
-int arr[501][501];
-int sum[501];
-
-int dp(int a, int b) {
-
-    if (a == b) {
-        return 0;
-    }
-    int& ret = arr[a][b];
-    if (ret) {
-        return arr[a][b];
-    }
-    ret = INT_MAX;
-
-    for (int m = a; m <= b - 1; m++) {
-        ret = min(arr[a][b], dp(a, m) + dp(m + 1, b) + sum[b] - sum[a- 1]);
-    }
-
-    return ret;
-}
-
-
-
 int main() {
-
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
 
     int t;
     cin >> t;
@@ -41,18 +12,30 @@ int main() {
     while(t--) {
         int k;
         cin >> k;
+        vector<int> sum(k + 1, 0);
+        int dp[501][501] = {0, };
 
         for (int i = 1; i <= k; i++) {
             int tmp;
             cin >> tmp;
             sum[i] = sum[i - 1] + tmp;
         }
-        int result = INT_MAX;
-        result = dp(1, k);
 
-        cout << result << '\n';
-        memset(arr, 0, sizeof(arr));
-        memset(sum, 0, sizeof(sum));
+        for (int i = 1; i < k; i++) {
+            for (int x = 1; x <= k - i; x++) {
+                int y = x + i;
+                dp[x][y] = INT_MAX;
+
+                for (int m = x; m < y; m++) {
+                    dp[x][y] = min(dp[x][y], dp[x][m] + dp[m + 1][y] + sum[y] - sum[x - 1]);
+
+                }
+            }
+
+        }
+
+        cout << dp[1][k] << '\n';
+        
     }
 
 }
