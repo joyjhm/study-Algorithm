f0c6457b0aa6627da724204b892c2bdbc593a76e
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-08-12 14:10:31 +0900

    [Gold II] Title: 공항, Time: 80 ms, Memory: 6644 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <set>
+using namespace std;
+
+
+int main() {
+
+    int g;
+    cin >> g;
+
+    int p;
+    cin >> p;
+
+    set<int> gate;
+
+    for (int i = 1; i <= g; i++) {
+        gate.insert(-i);;
+    }
+
+    int cnt = 0;
+
+    bool isEnd = false;
+
+    for (int i = 0; i < p; i++) {
+        int plane_num;
+        cin >> plane_num;
+
+        auto it = gate.lower_bound(-plane_num);
+        if (isEnd || it == gate.end()) {
+            isEnd = true;
+            continue;
+        }
+
+        gate.erase(it);
+        cnt++;
+
+    }
+
+    cout << cnt;
+}
