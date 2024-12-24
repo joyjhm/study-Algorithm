128e27a32c562fbe62a9492c49efdd7eb8121230
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-12-24 23:22:48 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+long long dp[5000];
+long long mod = 1000000;
+
+int main() {
+
+    string str;
+    cin >> str;
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    if(str[0] == '0') {
+        cout << 0;
+        return 0;
+    }
+
+    for (int i = 1; i < str.length(); i++) {
+        if (str[i] == '0' && (str[i - 1] == '1' || str[i - 1] == '2')) {
+            dp[i + 1] = dp[i - 1] % mod;
+        } 
+        else if (str[i] == '0' && (str[i - 1] == '0' || str[i - 1] >= '3')) {
+            cout << 0;
+            return 0;
+        }
+        else if ((str[i - 1] == '2' && str[i] <= '6' ) || (str[i - 1] == '1')) {
+            dp[i + 1] = (dp[i] + dp[i - 1]) % mod;
+        }
+        else {
+            dp[i + 1] = dp[i] % mod;
+        }
+
+    }
+
+    cout << dp[str.length()];
+
+}
